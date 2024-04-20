21e0fe7ce291c2cfac68fcccb12fcd7570c3f659
Federico Alvarez <fede3alvarez@gmail.com>
2024-04-20 11:36:58 -0400

    Refactoring

---
+++ HW_7_Term_Project/Face_Recognition.m
@@ -0,0 +1,7 @@
+load neural_network;
+
+test_images_folder = 'Test_Images';
+
+for test_image = 1:3
+    test_network(neural_network, image)
+end
---
+++ HW_7_Term_Project/NN_Creation_and_Training.m
@@ -0,0 +1,104 @@
+close all
+clear all;
+clc;
+
+% Code will be available at
+% https://github.com/fede3alvarez/ECE510_Biometrics
+% Homework 7 - Face Recognition
+
+
+%---------------------------------------
+%  Load Training Set
+%---------------------------------------
+
+% Load Training Set
+dataset_folder = "faces_training";
+dataset = imageDatastore(dataset_folder,...
+                       "IncludeSubfolders", true,...
+                       "LabelSource","foldernames");
+
+% Split set between training (30%) and validation (70%)
+%   Each folder / label / subject has ~20 pictures
+[training_dataset, validation_dataset] = splitEachLabel(dataset, 7.0);
+
+
+%---------------------------------------
+%  Create Neural Network (GoogleNet)
+%---------------------------------------
+
+% Create Neural Network
+%   The specific choice of Google Net is of not particular reason
+%   Documentation and examples were able AND
+%   Face Recognition is one of the targets of GoogleNet
+neural_network = googlenet;
+analyzeNetwork(neural_network);
+
+% Get Input size of 1st Layer
+input_layer_size = neural_network.Layers(1).InputSize;
+
+% Update Layers as needed
+layer_graph = layerGraph(neural_network);
+
+feature_learner = neural_network.Layers(142);
+output_classifier = neural_network.Layers(144);
+
+number_of_classes = numel(categories(training_dataset.Labels));
+
+new_feature_learner = fullyConnectedLayer(number_of_classes, ...
+                            "Name", "Facial Feature Learner",...
+                            "WeightLearnRateFactor", 10,...
+                            "BiasLearnRateFactor", 10);
+
+new_classifier_layer = classificationLayer("Name", "Face Classifier");
+
+layer_graph = replaceLayer(layer_graph, feature_learner.Name, new_feature_learner);
+layer_graph = replaceLayer(layer_graph, output_classifier.Name, new_classifier_layer);
+
+analyzeNetwork(layer_graph);
+
+%---------------------------------------
+%  Clean-up Image
+%---------------------------------------
+% 
+% pixel_range = [-30 30];
+% scale_range = [0.9 1.1];
+% 
+% image_augmented = imageDataAugmenter('RandXReflection', true,...
+%                                    'RandXTranslation', pixel_range,...
+%                                    'RandYTranslation', pixel_range,...
+%                                    'RandXScale', scale_range,...
+%                                    'RandyScale', scale_range);
+
+% Resize Image to fit within Neural Netwrok 
+augmented_training_image = augmentedImageDatastore(...
+                                        input_layer_size(1:2), ...
+                                        training_dataset);
+
+augmented_validation_image = augmentedImageDatastore(...
+                                        input_layer_size(1:2), ...
+                                        validation_dataset);
+
+%---------------------------------------
+%  Train Network 
+%---------------------------------------
+
+size_of_minibatch = 5;
+validation_frequency = floor(...
+           numel(augmented_training_image.Files) / size_of_minibatch...
+                             );
+
+training_options = trainingOptions("sgdm", ...
+                        "MiniBatchSize", size_of_minibatch,...
+                        "MaxEPochs", 6,...
+                        "InitialLearnRate", 3e-4,...
+                        "Shuffle", "every-epoch",...
+                        "ValidationData", augmented_validation_image,...
+                        "ValidationFrequency", validation_frequency,...
+                        "Verbose", false,...
+                        "Plots", "training-progress");
+
+neural_network = trainNetwork(augmented_training_image, ...
+                              layer_graph, ...
+                              training_options);
+
+save neural_network;
